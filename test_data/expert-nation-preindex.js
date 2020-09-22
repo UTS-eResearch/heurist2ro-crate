1e9aa3f43e51cca67e37159837edfd211950ea9e
Peter Sefton <peter.sefton@uts.edu.au>
2020-09-22 12:32:14 +1000

    ADded new preindex script to add _militaryServicePlace

---
+++ test_data/expert-nation-preindex.js
@@ -0,0 +1,40 @@
+const ROCrate = require("ro-crate").ROCrate;
+const program = require("commander");
+const { readFile } = require("fs-extra");
+const path = require("path");
+
+//
+var dir;
+program
+  .version("0.1.0")
+  .description(
+    "Pre-processes an ExpertNation RO-Crate to extract facet info"
+  )
+  .arguments("<i>")
+  .action((i) => {dir = i})
+program.parse(process.argv);
+
+
+async function addFacets(crate) {
+
+    crate.index();
+    for (let item of crate.getGraph()) {
+        if (item["@type"] === "MilitaryService") {
+            placesMatch = item.name.match(/.*\[\s+(.*)\].*/);
+            if (placesMatch) {
+                item._militaryServicePlace = placesMatch[1].split(/, */);
+            }
+        }
+    }
+    return crate;
+
+}
+
+//dir = program.dir ;
+
+async function main() {
+    const crate = new ROCrate(JSON.parse(await readFile(path.join(dir, "ro-crate-metadata.json"))));
+    const outputCrate = await addFacets(crate);
+    console.log(JSON.stringify(outputCrate, null, 2));
+}
+main();
