de3b16b28d6246002298555eda7ca445ad935ee7
Moises Sacal <moisbo@gmail.com>
2020-09-29 12:12:18 +1000

    added npm scripts to run it in en_config, it will create the en_crate and en_ocfl

--- en_config/expert-nation-preindex.js
+++ en_config/expert-nation-preindex.js
@@ -1,6 +1,6 @@
 const ROCrate = require("ro-crate").ROCrate;
 const program = require("commander");
-const { readFile } = require("fs-extra");
+const { readFile, writeFileSync } = require("fs-extra");
 const path = require("path");
 
 //
@@ -20,7 +20,7 @@ async function addFacets(crate) {
     crate.index();
     for (let item of crate.getGraph()) {
         if (item["@type"] === "MilitaryService") {
-            placesMatch = item.name.match(/.*\[\s*(.*?)\s*\].*/);
+            let placesMatch = item.name.match(/.*\[\s*(.*?)\s*\].*/);
             if (placesMatch) {
                 item._militaryServicePlace = placesMatch[1].split(/, */);
             }
@@ -35,6 +35,6 @@ async function addFacets(crate) {
 async function main() {
     const crate = new ROCrate(JSON.parse(await readFile(path.join(dir, "ro-crate-metadata.json"))));
     const outputCrate = await addFacets(crate);
-    console.log(JSON.stringify(outputCrate, null, 2));
+    writeFileSync(path.join(dir, "ro-crate-metadata.json"), JSON.stringify(outputCrate, null, 2));
 }
 main();
